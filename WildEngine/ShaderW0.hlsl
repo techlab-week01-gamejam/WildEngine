38a76483e6822bc295d70435baa4956919bf348d
Xerlocked <xhouse.xman@gmail.com>
2025-03-07 14:50:06 +0900

    first commit

---
+++ WildEngine/ShaderW0.hlsl
@@ -0,0 +1,39 @@
+// ShaderW0.hlsl
+cbuffer constants : register(b0)
+{
+    float3 Offset;
+    float Pad;
+    float4 ConstantColor;
+}
+
+struct VS_INPUT
+{
+    float4 position : POSITION; // Input position from vertex buffer
+    float4 color : COLOR;       // Input color from vertex buffer
+};
+
+struct PS_INPUT
+{
+    float4 position : SV_POSITION; // Transformed position to pass to the pixel shader
+    float4 color : COLOR;          // Color to pass to the pixel shader
+};
+
+PS_INPUT mainVS(VS_INPUT input)
+{
+    PS_INPUT output;
+    
+    // Pass the position directly to the pixel shader (no transformation)
+    output.position = input.position * Pad + float4(Offset, 1); // float4(Offset, 0) + input.position * Pad;
+    
+    
+    // Pass the color to the pixel shader
+    output.color = input.color * ConstantColor;
+    
+    return output;
+}
+
+float4 mainPS(PS_INPUT input) : SV_TARGET
+{
+    // Output the color directly
+    return input.color;
+}
